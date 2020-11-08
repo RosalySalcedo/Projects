682fcec3806484d3c1220015a72bc99c2045d66c
Rosaly Salcedo <rsalcedo@syr.edu>
2020-11-08 01:53:11 -0500

    Update letters_random.py

--- Useful Python Script/Random/letters_random.py
+++ Useful Python Script/Random/letters_random.py
@@ -1,6 +1,7 @@
 import random, string
 
 def random_char(y):
+     '''Insert how many random letters.'''
     return ''.join(random.choice(string.ascii_letters) for x in range(y))
 
 print (random_char(5))
